6abb1c8aeb7805cf6de27dde698f849d3bcea3f4
sugiwa <sugi.warra@gmail.com>
2020-10-13 18:31:37 +0900

    20201013

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTwitter, faFacebookSquare, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 export default () =>(
     <footer className="footer">
@@ -8,21 +8,9 @@ export default () =>(
 
             <ul className="sns">
                 <li>
-                <a href="https://twitter.com/">
+                <a href="https://twitter.com/waka_pre">
                     <FontAwesomeIcon icon={faTwitter} />
                     <span className="sr-only">Twitter</span>
-                </a>
-                </li>
-                <li>
-                <a href="https://facebook.com/">
-                    <FontAwesomeIcon icon={faFacebookSquare} />
-                    <span className="sr-only">Facebook</span>
-                </a>
-                </li>
-                <li>
-                <a href="http://instagram.com/">
-                    <FontAwesomeIcon icon={faInstagram} />
-                    <span className="sr-only">Instagram</span>
                 </a>
                 </li>
             </ul>
