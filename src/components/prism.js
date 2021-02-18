739a04a5420e434f35654deebdb3d442fa30e0d6
sugiwa <sugi.warra@gmail.com>
2021-02-19 01:25:00 +0900

    add highlight language

--- src/components/prism.js
+++ src/components/prism.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react'
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-okaidia.css'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-powershell'
+import 'prismjs/components/prism-git'
 
 export default ({langname, children}) => {
   useEffect(() => {
